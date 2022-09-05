fa733645d8e95cb7301565ce8412390c3a16c9ec
ElsayedTork <elsayed.torky@inovola.com>
2022-09-05 10:54:45 +0200

    clean up store

--- src/Store/index.js
+++ src/Store/index.js
@@ -12,22 +12,17 @@
             state.certifications = certifications;
         }
      },
-//     getters: {
-//       getUsers: (state) => state.formData
-//     },
 
    actions:{
     actionCertifications({commit}){
         axios.get('https://inovola-stage.com/tahara-backend/api/reviews').then(response => {
             commit('setCertifications',response.data.data);
-            console.log("Actionsss",response.data.data)
         });
              
              
     },
          sendDataForm(_, formData){
              axios.post('https://inovola-stage.com/tahara-backend/api/send-contact',this.formData)
-             console.log('from Action',formData)
      }
     
     }
